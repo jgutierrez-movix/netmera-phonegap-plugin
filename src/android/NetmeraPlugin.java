98be3be342c92342cb27c7487c1fcf556fc74048
Serdar Kuzucu <serdarkuzucu13@gmail.com>
2015-05-11 16:04:55 +0300

    added addTags, overrideTags, and sendEvent functionalities

--- src/android/NetmeraPlugin.java
+++ src/android/NetmeraPlugin.java
@@ -1,87 +1,113 @@
 package org.apache.cordova.core;
 
-import android.app.Activity;
-import android.app.Application;
-import android.util.Log;
-
-import java.util.Set;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
-import com.netmera.mobile.Netmera;
-import com.netmera.mobile.NetmeraProperties;
-import com.netmera.mobile.NetmeraProperties.Builder;
+import android.app.Activity;
+import android.app.Application;
+import android.util.Log;
+
 import com.netmera.mobile.Logging.LogLevel;
+import com.netmera.mobile.Netmera;
 import com.netmera.mobile.NetmeraDeviceDetail;
-import com.netmera.mobile.NetmeraPushService;
+import com.netmera.mobile.NetmeraEvent;
 import com.netmera.mobile.NetmeraException;
+import com.netmera.mobile.NetmeraProperties;
+import com.netmera.mobile.NetmeraPushService;
 
 public class NetmeraPlugin extends CordovaPlugin {
-    public static final String TAG = "NetmeraPlugin";
-    public static final String ACTION_REGISTER = "register";
-	
+	public static final String TAG = "NetmeraPlugin";
+	public static final String ACTION_SET_TAGS = "setTags";
+	public static final String ACTION_SEND_EVENT = "sendCustomEvent";
+
 	private static Application app;
 	private static Class<? extends Activity> pushActivityClass;
 	private static String googleProjectId;
 
-    public static void initNetmera(Application app, Class<? extends Activity> pushActivityClass) {
+	public static void initNetmera(Application app, Class<? extends Activity> pushActivityClass) {
 		NetmeraPlugin.app = app;
 		NetmeraPlugin.pushActivityClass = pushActivityClass;
-		
-        String apiKey = getStringByKey(app, "netmera_api_key");
-        NetmeraPlugin.googleProjectId = getStringByKey(app, "netmera_google_project_id");
-        
-        Log.d(TAG, "Initializing Netmera.. ApiKey: " + apiKey + " and googleProjectId:" + googleProjectId);
-		
-		NetmeraProperties prop = new NetmeraProperties.Builder(apiKey)
-			.googleProjectNumber(googleProjectId)
-			.pushActivityClass(pushActivityClass)
-			.exceptionReportingEnabled(true)
-			.logLevel(LogLevel.INFO)
-			.locationTrackingEnabled(true)
-			.exceptionReportingEnabled(true)
-			.pushInboxEnabled(false)
-			.popupEnabled(false)
-			.build();
+
+		String apiKey = getStringByKey(app, "netmera_api_key");
+		NetmeraPlugin.googleProjectId = getStringByKey(app, "netmera_google_project_id");
+
+		Log.d(TAG, "Initializing Netmera.. ApiKey: " + apiKey + " and googleProjectId:" + googleProjectId);
+
+		NetmeraProperties prop = new NetmeraProperties.Builder(apiKey).googleProjectNumber(googleProjectId).pushActivityClass(pushActivityClass).exceptionReportingEnabled(true)
+				.logLevel(LogLevel.INFO).locationTrackingEnabled(true).exceptionReportingEnabled(true).pushInboxEnabled(false).popupEnabled(false).build();
 		Netmera.init(app, prop);
-    }
+	}
 
-    private static String getStringByKey(Application app, String key) {
-        int resourceId = app.getResources().getIdentifier(key, "string", app.getPackageName());
-        return app.getString(resourceId);
-    }
+	private static String getStringByKey(Application app, String key) {
+		int resourceId = app.getResources().getIdentifier(key, "string", app.getPackageName());
+		return app.getString(resourceId);
+	}
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if (action.equals(ACTION_REGISTER)) {
-            this.register(args, callbackContext);
-            return true;
-        }
-		
-        return false;
-    }
-	
-	private void register(final JSONArray args, final CallbackContext callbackContext) {
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals(ACTION_SET_TAGS)) {
+			this.setTags(convertJSONArrayToStringList(args.getJSONArray(0)), args.getBoolean(1), callbackContext);
+			return true;
+		}
+
+		if (action.equals(ACTION_SEND_EVENT)) {
+			if (args.length() == 1) {
+				this.sendCustomEvent(args.getString(0), null, callbackContext);
+				return true;
+			} else if (args.length() == 2) {
+				this.sendCustomEvent(args.getString(0), args.getJSONObject(1), callbackContext);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private void setTags(final List<String> tags, final boolean overrideTags, final CallbackContext callbackContext) {
 		cordova.getThreadPool().execute(new Runnable() {
-            public void run() {
+			public void run() {
 				NetmeraDeviceDetail deviceDetail = new NetmeraDeviceDetail(app.getApplicationContext(), googleProjectId, pushActivityClass);
-				List<String> tags = new ArrayList<String>();
-				tags.add("News");
-				tags.add("Sports");
 				deviceDetail.setTags(tags);
+				deviceDetail.setOverrideTags(overrideTags);
 				try {
 					NetmeraPushService.register(deviceDetail);
+					callbackContext.success();
 				} catch (NetmeraException e) {
-					// Handle exception
+					callbackContext.error(e.getMessage());
 				}
-				callbackContext.success();
 			}
 		});
 	}
-}
 
+	private void sendCustomEvent(final String eventName, final JSONObject eventData, final CallbackContext callbackContext) {
+		cordova.getThreadPool().execute(new Runnable() {
+			public void run() {
+				try {
+					if (eventData == null) {
+						NetmeraEvent.sendCustomEvent(eventName);
+						callbackContext.success();
+					} else {
+						NetmeraEvent.sendCustomEventWithData(eventName, eventData);
+						callbackContext.success();
+					}
+				} catch (NetmeraException e) {
+					callbackContext.error(e.getMessage());
+				}
+			}
+		});
+	}
+
+	private List<String> convertJSONArrayToStringList(JSONArray jsonArray) throws JSONException {
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < jsonArray.length(); i++) {
+			list.add(jsonArray.getString(i));
+		}
+		return list;
+	}
+}
--- www/netmera-phonegap-plugin.js
+++ www/netmera-phonegap-plugin.js
@@ -1,4 +1,42 @@
 var netmeraPlugin = {
-    
+	addTags: function (tags, successCallback, errorCallback) {
+		cordova.exec(
+			successCallback,
+			errorCallback,
+			'NetmeraPlugin',
+			'setTags',
+			[tags, false]
+		);
+	},
+	
+	overrideTags: function (tags, successCallback, errorCallback) {
+		cordova.exec(
+			successCallback,
+			errorCallback,
+			'NetmeraPlugin',
+			'setTags',
+			[tags, true]
+		);
+	},
+	
+	sendEvent: function (eventName, successCallback, errorCallback) {
+		cordova.exec(
+			successCallback,
+			errorCallback,
+			'NetmeraPlugin',
+			'sendCustomEvent',
+			[eventName]
+		);
+	},
+	
+	sendEventWithData: function (eventName, eventData, successCallback, errorCallback) {
+		cordova.exec(
+			successCallback,
+			errorCallback,
+			'NetmeraPlugin',
+			'sendCustomEvent',
+			[eventName, eventData]
+		);
+	}
 };
 module.exports = netmeraPlugin;
