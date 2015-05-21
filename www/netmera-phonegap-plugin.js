3a354c85f896ac9468dbfcb2edb40d5aac47247b
ufser <ufukserdogan@gmail.com>
2015-05-21 15:25:59 +0300

    Updated

--- www/netmera-phonegap-plugin.js
+++ www/netmera-phonegap-plugin.js
@@ -39,6 +39,16 @@ var netmeraPlugin = {
 		);
 	},
 	
+	getTags: function (successCallback, errorCallback) {
+		cordova.exec(
+			successCalback,
+			errorCallback,
+			'NetmeraPlugin',
+			'getTags',
+			[]
+			);
+	},
+	
 	sendEvent: function (eventName, successCallback, errorCallback) {
 		console.log("sending event: " + eventName);
 		cordova.exec(
@@ -80,6 +90,16 @@ var netmeraPlugin = {
 		);
 	},
 	
+	overrideCustomFields: function (customFields, successCallback, errorCallback) {
+		cordova.exec(
+			successCallback,
+			errorCallback,
+			'NetmeraPlugin',
+			'overrideCustomFields',
+			[customFields]
+		);	
+	},
+	
 	unregister: function (successCallback, errorCallback) {
 		cordova.exec(
 			successCallback,
@@ -108,6 +128,16 @@ var netmeraPlugin = {
 			'getDeviceDetail',
 			[]
 		);
-	}
+	},
+	
+	getInstallationId: function ()  {
+		cordova.exec(
+			successCallback,
+			errorCallback,
+			'NetmeraPlugin',
+			'getInstallationId',
+			[]
+		);
+	},
 };
 module.exports = netmeraPlugin;
